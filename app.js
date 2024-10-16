e17a8bdec4fc287aeeefdf1702cc10455272bd52
Yagnik Akbari <yagnik58ppsv@gmail.com>
2024-10-16 08:05:10 +0530

    Request accept page changes and add base setup for soket io connection for message between to users.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const multer = require("multer");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const User = require("./model/user");
 
@@ -14,6 +16,8 @@ const MONGODB_URL =
 
 mongoose.set("strictQuery", false);
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 app.set("view engine", "ejs");
 
 const authRoutes = require("./routes/auth");
@@ -30,6 +34,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/socket.io",
+  express.static(__dirname + "/node_modules/socket.io/client-dist")
+);
 
 app.use(
   session({
@@ -42,6 +50,23 @@ app.use(
     },
   })
 );
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Listen for messages from clients
+  socket.on("message", (data) => {
+    console.log("Message received:", data);
+
+    // Emit the message to all clients
+    io.emit("message", data);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 app.use(authRoutes);
 app.use(needyRoutes);
@@ -63,25 +88,25 @@ app.use((req, res, next) => {
   }
 
   User.findById(req.session.user._id)
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return next();
       }
       req.user = user;
       next();
     })
-    .catch(err => {
+    .catch((err) => {
       throw new Error(err);
     });
 });
 
 mongoose
   .connect(MONGODB_URL)
-  .then(db => {
+  .then((db) => {
     app.listen(3000);
     console.log("Database is Connected. PORT=3000");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
     console.log("Database not connected!");
   });
